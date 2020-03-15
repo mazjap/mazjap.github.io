e86cf323f0addf8f7e8a831bef15872340dd90df
Jordan Christensen <jordan.c4922@gmail.com>
2020-03-14 22:25:13 -0600

    Change colors again, add technologies to projects

--- js/script.js
+++ js/script.js
@@ -32,68 +32,67 @@ $(document).ready(function() {
     })
 })
 
-    var project1_slide_index = 1; displaySlides(project1_slide_index, 1)
-    var project2_slide_index = 1; displaySlides(project2_slide_index, 2)
-    var project3_slide_index = 1; displaySlides(project3_slide_index, 3)
+$(window).scroll(function() {
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+    
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+   
+    $panel.each(function () {
+      var $this = $(this);
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+        });
 
-    function nextSlide(val, num) {
-        if (num == 1) {
-            displaySlides(project1_slide_index += val, num)
-        } else if (num == 2) {
-            displaySlides(project2_slide_index += val, num)
-        } else if (num == 3) {
-            displaySlides(project3_slide_index += val, num)
-        }
+        $body.addClass('color-' + $(this).data('color'));
+      }
+    });    
+    
+  }).scroll();
+
+var project1_slide_index = 1; displaySlides(project1_slide_index, 1)
+var project2_slide_index = 1; displaySlides(project2_slide_index, 2)
+var project3_slide_index = 1; displaySlides(project3_slide_index, 3)
+
+function nextSlide(val, num) {
+    if (num == 1) {
+        displaySlides(project1_slide_index += val, num)
+    } else if (num == 2) {
+        displaySlides(project2_slide_index += val, num)
+    } else if (num == 3) {
+        displaySlides(project3_slide_index += val, num)
     }
+}
 
-    function currentSlide(val, num) {
-        if (num == 1) {
-            displaySlides(project1_slide_index = val, num)
-        } else if (num == 2) {
-            displaySlides(project2_slide_index = val, num)
-        } else if (num == 3) {
-            displaySlides(project3_slide_index = val, num)
-        }
+function currentSlide(val, num) {
+    if (num == 1) {
+        displaySlides(project1_slide_index = val, num)
+    } else if (num == 2) {
+        displaySlides(project2_slide_index = val, num)
+    } else if (num == 3) {
+        displaySlides(project3_slide_index = val, num)
     }
+}
 
-    function displaySlides(num, projectNum) {
-        var i
-        var slides = document.getElementsByClassName("project".concat(projectNum, "_showSlide"))
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none"
-        }
-        if (projectNum == 1) {
-            if (num > slides.length) { project1_slide_index = 1 }
-            if (num < 1) { project1_slide_index = slides.length }
-            slides[project1_slide_index - 1].style.display = "block"
-        } else if (projectNum == 2) {
-            if (num > slides.length) { project2_slide_index = 1 }
-            if (num < 1) { project2_slide_index = slides.length }
-            slides[project2_slide_index - 1].style.display = "block"
-        } else if (projectNum == 3) {
-            if (num > slides.length) { project3_slide_index = 1 }
-            if (num < 1) { project3_slide_index = slides.length }
-            slides[project3_slide_index - 1].style.display = "block"
-        }
+function displaySlides(num, projectNum) {
+    var i
+    var slides = document.getElementsByClassName("project".concat(projectNum, "_showSlide"))
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
     }
-
-
-    $(window).scroll(function() {
-        var $window = $(window),
-            $body = $('body'),
-            $panel = $('.panel');
-        
-        var scroll = $window.scrollTop() + ($window.height() / 3);
-       
-        $panel.each(function () {
-          var $this = $(this);
-          if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            $body.removeClass(function (index, css) {
-              return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-            });
-
-            $body.addClass('color-' + $(this).data('color'));
-          }
-        });    
-        
-      }).scroll();
+    if (projectNum == 1) {
+        if (num > slides.length) { project1_slide_index = 1 }
+        if (num < 1) { project1_slide_index = slides.length }
+        slides[project1_slide_index - 1].style.display = "block"
+    } else if (projectNum == 2) {
+        if (num > slides.length) { project2_slide_index = 1 }
+        if (num < 1) { project2_slide_index = slides.length }
+        slides[project2_slide_index - 1].style.display = "block"
+    } else if (projectNum == 3) {
+        if (num > slides.length) { project3_slide_index = 1 }
+        if (num < 1) { project3_slide_index = slides.length }
+        slides[project3_slide_index - 1].style.display = "block"
+    }
+}
